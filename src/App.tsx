7109565eb8618cf76512f043864fdb59313f26d4
DIPU-pixel <dipankarkb12@gmail.com>
2026-01-25 00:08:06 +0530

    making the floder and react js conversation to typscript

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const App = () => {
+  return (
+ <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-green-600">
+        React + Tailwind + TypeScript ✅
+      </h1>
+    </div>
+  );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
  content: [
     "./index.html",
-    "./src/**/*.{js,jsx}"
+    "./src/**/*.{ts,tsx}"
   ],
   theme: {
     extend: {},
